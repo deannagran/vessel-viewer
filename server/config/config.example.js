ceda9625057742bc5ba30264f7724a99aacc96cb
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2020-10-13 00:41:33 -0400

    Update config.example.js

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -4,5 +4,8 @@
 module.exports = {
     db: {
         uri: '', //place the URI of your mongo database here.
+    },
+    jwt: {
+        JWT_SECRET: ' ', // this is the secret server token password-- can be anything you want when you run locally
     }
 };
