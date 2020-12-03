edbc934c4570fdc3488f47519aabfab9d6038a7e
joshuaaird <60545486+joshuaaird@users.noreply.github.com>
2020-12-02 21:28:20 -0500

    contact page
    with new css styling

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -6,5 +6,68 @@ export default function Contact(props) {
   useEffect(() => {
     if (!userData.user) props.history.push("/login");
   });
-  return <div className="page"><h1>Contact Digital Twin Marine</h1></div>;
+  if (userData.user){
+    var cname = userData.user.firstName + userData.user.lastName;
+  } else {
+    var cname = "";
+  }
+  if (userData.user){
+  return (
+  <div className="page">
+    <h1>Contact Digital Twin Marine</h1>
+    <form id="contact-form" /*onSubmit={handleSubmit.bind(this)} method="POST"*/>
+    <div className="contactlabel">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="contactinput" defaultValue={cname} />
+    </div>
+    <div className="contactlabel">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input type="email" className="contactinput" aria-describedby="emailHelp" defaultValue={userData.user.email} />
+    </div>
+    <div className="contactlabel">
+        <label htmlFor="name">Phone Number (Optional)</label>
+        <input type="text" className="contactinput" />
+    </div>
+    <div className="contactlabel">
+        <label htmlFor="message">Company (If Applicable)</label>
+        <input type="text" className="contactinput"  />
+    </div>
+    <div className="contactlabel">
+        <label htmlFor="message">Message</label>
+        <textarea className="contactinput-message" rows="5"></textarea>
+    </div>
+    <button type="submit" className="contactbutton">Submit</button>
+    </form> 
+  </div>
+  );
+  } else{
+    return (
+      <div className="page">
+      <h1>Contact Digital Twin Marine</h1>
+      <form id="contact-form" /*onSubmit={handleSubmit.bind(this)} method="POST"*/>
+      <div className="contactlabel">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="contactinput"  />
+      </div>
+      <div className="contactlabel">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="contactinput" aria-describedby="emailHelp" />
+      </div>
+      <div className="contactlabel">
+          <label htmlFor="name">Phone Number (Optional)</label>
+          <input type="text" className="contactinput" />
+      </div>
+      <div className="contactlabel">
+          <label htmlFor="message">Company (If Applicable)</label>
+          <input type="text" className="contactinput"  />
+      </div>
+      <div className="contactlabel">
+          <label htmlFor="message">Message</label>
+          <textarea className="contactinput-message" rows="5"></textarea>
+      </div>
+      <button type="submit" className="contactbutton">Submit</button>
+      </form> 
+    </div>
+    );
+  }
 }
