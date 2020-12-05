fecfeb17863508f4354bc25a102842acd30e3e7d
joshuaaird <60545486+joshuaaird@users.noreply.github.com>
2020-12-05 18:46:05 -0500

    Bootstrap forms update

--- client/src/views/auth/Login.js
+++ client/src/views/auth/Login.js
@@ -5,6 +5,7 @@ import Axios from "axios";
 import ErrorNotice from "../misc/ErrorNotice";
 import { Link } from "react-router-dom"
 
+
 export default function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
@@ -32,7 +33,62 @@ export default function Login() {
     }
   };
   return (
-    <div className="page">
+
+	<div id ="root">
+	<div className="limiter">
+		<div className="container-login100">
+			<div className="wrap-login100">
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
+				<form className="login100-form validate-form p-l-55 p-r-55 p-t-140" onSubmit={submit}>
+					<span className="login100-form-title">
+						Sign In
+					</span>
+
+					<div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Email Address">
+						<input className="input100" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+						<span className="focus-input100"></span>
+					</div>
+
+					<div className="wrap-input100 validate-input" data-validate = "Please enter password">
+						<input className="input100" type="password" name="pass" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+						<span className="focus-input100"></span>
+					</div>
+
+					<div className="text-right p-t-13 p-b-23">
+						<span className="txt1">
+							Forgot 
+						</span>
+
+						<a href="#" className="txt2">
+							Email / Password?
+						</a>
+					</div>
+
+					<div className="container-login100-form-btn">
+						<button className="login100-form-btn" onClick={submit}>
+							Sign in
+						</button>
+					</div>
+
+					<div className="flex-col-c p-t-90 p-b-30">
+						<span className="txt1 p-b-9">
+							Don’t have an account?
+						</span>
+
+						<a href="/register" className="txt3">
+							Register
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+  </div>
+  );
+}
+/*<div className="page">
       <h2>Returning User Login</h2>
       {error && (
         <ErrorNotice message={error} clearError={() => setError(undefined)} />
@@ -59,6 +115,4 @@ export default function Login() {
         to register.
         </label>
       </form>
-    </div>
-  );
-}
+    </div>*/
