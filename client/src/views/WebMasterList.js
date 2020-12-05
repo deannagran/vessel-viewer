9f56468799ca0a637658d2184227db4ca56cfe86
jose.jon <josejon@users.noreply.github.com>
2020-12-05 18:59:06 -0500

    Update WebMasterList.js

--- client/src/views/WebMasterList.js
+++ client/src/views/WebMasterList.js
@@ -35,18 +35,7 @@ const WebMasterList = () => {
         console.log(id);
         axiosdeleteVessel(id);
         axiosGetVessels();
-        setOpen(true);
-        return(
-            <div>
-            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                <h2>Are you sure you want to delete this Vessel</h2>
-                    <button class="register-button " type="webMasterList-button" onClick={deleteVessel}>
-                        Yes
-                    </button>
-                    <button type="button" className="webMasterList-button" onClick={closeModal}>No</button>
-            </Popup>
-            </div>
-        );
+        setOpen(false);
     };
 
     const updateVessels = () => {axiosGetVessels();};
@@ -56,19 +45,31 @@ const WebMasterList = () => {
     };
 
     if(vesselArray.length > 0){
-        let listOfVessels = vesselArray.map(vessel =>
+        /*let listOfVessels = vesselArray.map(vessel =>
           `<h3 class="card-title">${vessel.name} 
-          <button id="${vessel.name}"  class="webMasterList-button">Delete</button> 
+          <button id="${vessel.name}" onClick={deleteVessel} class="webMasterList-button">Delete</button> 
           </h3>`
-        ).join('');
+        ).join('');*/
     
         return (
         <div className="page">
             <button type="button" className="register-button" onClick={updateVessels}>Update</button>
         <h2>List of Vessels</h2>
           <table border = "0" cellPadding = "25" cellSpacing = "10"> 
-            <div  onClick={() => setOpen(o => !o)} dangerouslySetInnerHTML={{__html: listOfVessels}}></div>
-            
+          <div>
+            {vesselArray.map(vessel =>
+            <h3 class="card-title">{vessel.name} 
+            <button id={vessel.name} onClick={() => setOpen(o => !o)} class="webMasterList-button">Delete</button> 
+            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+                <h2>Are you sure you want to delete this Vessel</h2>
+                    <button class="register-button " type="webMasterList-button" onClick={deleteVessel}>
+                        Yes
+                    </button>
+                    <button type="button" className="webMasterList-button" onClick={closeModal}>No</button>
+            </Popup>
+            </h3>
+            )}
+            </div>
           </table>
         </div>);
     }
