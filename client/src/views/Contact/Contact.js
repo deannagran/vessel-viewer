7584a61b5e5e2651f2702f1d9cd37152ae2877fd
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2020-12-05 16:52:53 -0500

    fixed alert styling for contact page

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../../context/UserContext";
 import Axios from "axios";
+import Alert from 'react-bootstrap/Alert'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Contact(props) {
+
+  const [show, setShow] = useState(true);
   const { userData } = useContext(UserContext);
   useEffect(() => {
     if (!userData.user) props.history.push("/login");
@@ -20,41 +24,77 @@ export default function Contact(props) {
 
   const sent = () => 
   {
-    alert("Your message has been sent successfully!"); 
+    //alert("Your message has been sent successfully!"); 
+    setShow(true);
+    
   }
 
   if (userData.user){
-  return (
-  <div className="page">
-    <h1>Contact Digital Twin Marine</h1>
-    <form id="contact-form" action = "/users/sendContact" method = "post">
-    <div className="contactlabel">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="contactinput" defaultValue={cname}
-         name = "user_name" />
-    </div>
-    <div className="contactlabel">
-        <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="contactinput" name = "user_email"
-        aria-describedby="emailHelp" defaultValue={userData.user.email} />
-    </div>
-    <div className="contactlabel">
-        <label htmlFor="name">Phone Number (Optional)</label>
-        <input type="text" className="contactinput" name = "user_phone" />
-    </div>
-    <div className="contactlabel">
-        <label htmlFor="message">Company (If Applicable)</label>
-        <input type="text" className="contactinput" name = "user_company" />
-    </div>
-    <div className="contactlabel">
-        <label htmlFor="message">Message</label>
-        <textarea className="contactinput-message" name = "user_message" 
-        rows="5"></textarea>
-    </div>
-    <button type="submit" onClick = {sent}  className="contactbutton">Submit</button>
-    </form> 
-  </div>
-  );
+    if(show){
+      return(<div className="page"> <Alert variant="success" onClose={() => setShow(false)} dismissible>
+      <Alert.Heading>Your message has been sent successfully!</Alert.Heading>
+      </Alert> <h1>Contact Digital Twin Marine</h1>
+          <form id="contact-form" action = "/users/sendContact" method = "post">
+          <div className="contactlabel">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="contactinput" defaultValue={cname}
+               name = "user_name" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="contactinput" name = "user_email"
+              aria-describedby="emailHelp" defaultValue={userData.user.email} />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="name">Phone Number (Optional)</label>
+              <input type="text" className="contactinput" name = "user_phone" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="message">Company (If Applicable)</label>
+              <input type="text" className="contactinput" name = "user_company" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="message">Message</label>
+              <textarea className="contactinput-message" name = "user_message" 
+              rows="5"></textarea>
+          </div>
+          <button type="submit" onClick = {sent}  className="contactbutton">Submit</button>
+          </form> 
+        </div>);
+      
+    }else{
+      return (
+        <div className="page">
+          <h1>Contact Digital Twin Marine</h1>
+          <form id="contact-form" action = "/users/sendContact" method = "post">
+          <div className="contactlabel">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="contactinput" defaultValue={cname}
+               name = "user_name" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="contactinput" name = "user_email"
+              aria-describedby="emailHelp" defaultValue={userData.user.email} />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="name">Phone Number (Optional)</label>
+              <input type="text" className="contactinput" name = "user_phone" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="message">Company (If Applicable)</label>
+              <input type="text" className="contactinput" name = "user_company" />
+          </div>
+          <div className="contactlabel">
+              <label htmlFor="message">Message</label>
+              <textarea className="contactinput-message" name = "user_message" 
+              rows="5"></textarea>
+          </div>
+          <button type="submit" onClick = {sent}  className="contactbutton">Submit</button>
+          </form> 
+        </div>
+        );
+    }
   } else{
     return (
       <div className="page">
