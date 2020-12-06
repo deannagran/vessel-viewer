beb5d148bb3351d4cf1466b5c87d25c4c79591a0
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2020-12-05 19:12:59 -0500

    styling on dashboard and added footer

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./views/NotFound";
 import ProjectPage from "./views/ProjectPage";
 //import NavBar from "./components/Header/NavBar";
 import Header from "./views/SiteHeader";
+import Footer from "./views/SiteFooter";
 import Confirmation from "./views/Confirmation";
 import Login from "./views/auth/Login";
 import Dashboard from "./views/Dashboard/Dashboard";
@@ -56,6 +57,7 @@ export default function App() {
       <BrowserRouter>
         <UserContext.Provider value={{ userData, setUserData }}>
           <Header />
+          <Footer />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Welcome} />          
---
+++ client/src/views/SiteFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AuthOptions from "./auth/AuthOptions";
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function SiteFooter() {
+  return (
+<div class="footer">
+
+  
+  <i class="pull-right"><img src="https://digitaltwinmarine.com/wp-content/uploads/2020/07/DTM-Logo-NoWords.png"></img></i><p>contact@digitaltwinmarine.com <br></br>
+         +1 (954) 686-7011<br></br>
+         2598 E. Sunrise Blvd, Suite 2104, Fort Lauderdale, FL 33304</p>
+</div>
+  );
+}
