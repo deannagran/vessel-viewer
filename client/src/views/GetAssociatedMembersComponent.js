e39e6839f92bc13100738d46358e362287e6f022
Amer Khalifa <68618099+amerkhalifa@users.noreply.github.com>
2020-12-06 11:30:53 -0500

    able to get entire bootstrap to show
    Nothing is connected to my users yet though

--- client/src/views/GetAssociatedMembersComponent.js
+++ client/src/views/GetAssociatedMembersComponent.js
@@ -97,14 +97,144 @@ import {useHistory} from "react-router-dom";
             console.log(memberArray.filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i));
             if (userData.currVessel && memberArrayCopy.length === userData.currVessel.associatedUsers.length) {
                 let listOfMembers = memberArrayCopy.map(member =>
-                    `<div class="card bg-light mb-3" style="width: 18rem">
-              <img class="card-img-top" src="" alt="Card image">
-              <div class="card-body">
-              <h3 class="card-title">${member.fName + " " + member.lName}</h3>
-              <button id="${member.memberID}" class="register-button ">Update User Roles</button> 
-              <br>
-              </div>
-              </div>
+                    `<link rel="stylesheet" type="text/css"
+                          href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+                        <hr>
+                            <div className="container bootstrap snippets bootdey">
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="main-box no-header clearfix">
+                                            <div className="main-box-body clearfix">
+                                                <div className="table-responsive">
+                                                    <table className="table user-list">
+                                                        <thead>
+                                                        <tr>
+                                                            <th><span>User</span></th>
+                                                            <th><span>Created</span></th>
+                                                            <th className="text-center"><span>Status</span></th>
+                                                            <th><span>Email</span></th>
+                                                            <th>&nbsp;</th>
+                                                        </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        <tr>
+                                                            <td>
+                                                                <img src="https://bootdey.com/img/Content/user_1.jpg"
+                                                                     alt="">
+                                                                    <a href="#" className="user-link">Full name 1</a>
+                                                                    <span className="user-subhead">Member</span>
+                                                                </img>
+                                                            </td>
+                                                            <td>2013/08/12</td>
+                                                            <td className="text-center">
+                                                                <span className="label label-default">pending</span>
+                                                            </td>
+                                                            <td>
+                                                                <a href="#">marlon@brando.com</a>
+                                                            </td>
+                                                            <td style="width: 20%;">
+                                                                <a href="#" className="table-link text-warning">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link text-info">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link danger">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                <img src="https://bootdey.com/img/Content/user_3.jpg"
+                                                                     alt="">
+                                                                    <a href="#" className="user-link">Full name 2</a>
+                                                                    <span className="user-subhead">Admin</span>
+                                                                </img>
+                                                            </td>
+                                                            <td>2013/08/12</td>
+                                                            <td className="text-center">
+                                                                <span className="label label-success">Active</span>
+                                                            </td>
+                                                            <td>
+                                                                <a href="#">marlon@brando.com</a>
+                                                            </td>
+                                                            <td style="width: 20%;">
+                                                                <a href="#" className="table-link  text-warning">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link  text-info">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link danger">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                <img src="https://bootdey.com/img/Content/user_2.jpg"
+                                                                     alt="">
+                                                                    <a href="#" className="user-link">Full name 3</a>
+                                                                    <span className="user-subhead">Member</span>
+                                                                </img>
+                                                            </td>
+                                                            <td>2013/08/12</td>
+                                                            <td className="text-center">
+                                                                <span className="label label-danger">inactive</span>
+                                                            </td>
+                                                            <td>
+                                                                <a href="#">marlon@brando.com</a>
+                                                            </td>
+                                                            <td style="width: 20%;">
+                                                                <a href="#" className="table-link  text-warning">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link  text-info">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                                <a href="#" className="table-link danger">
+                                            <span className="fa-stack">
+                                                <i className="fa fa-square fa-stack-2x"></i>
+                                                <i className="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </hr>
+                    </link>
               <br>
        
       `
