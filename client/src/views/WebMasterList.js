838f301c00a539c1274a39408bef66100277b11e
Amer Khalifa <68618099+amerkhalifa@users.noreply.github.com>
2020-12-07 16:23:45 -0500

    Revert "Revert "webmaster portal functional""
    This reverts commit db19376e06708659bdb07fd7900113248d0d5fc6.

--- client/src/views/WebMasterList.js
+++ client/src/views/WebMasterList.js
@@ -1,43 +1,74 @@
-import React from "react";
-import UserContext from "../context/UserContext";
+import React, { useEffect, useContext, useState } from "react";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import Axios from "axios";
 
 const WebMasterList = () => {
-    /*const [vesselName, setVesselName] = useState(0);
-    setVesselName(""+associatedVessels.data.retInfo);
-    const buildingList =
-    true
-    ? props.data.map((footballClub) => {
+    const [vesselArray, setVesselArray] = useState([]);
+    const [vesselName, setVesselName] = useState(0);
+    let axiosdeleteVessel;
+    let axiosGetVessels;
+    useEffect(() => { 
+    let routeResponse;
+    
+    axiosGetVessels = async () => {
+        console.log("getting vessels");
+        routeResponse = await Axios.post("http://localhost:5000/users/webMasterList"); 
+        
+        if(routeResponse){
+            setVesselArray(routeResponse.data.docArray);
+            console.log(routeResponse.data.docArray);
+        }else{
+            //console.log("no reply");
+        }
+        
+    }
+    axiosdeleteVessel = async (vesselid) => {
+        //console.log("deleteVessel" + vesselid);
+        let routeResponse2 = await Axios.post("http://localhost:5000/users/webMasterListDelete",
+        { 
+          name: vesselid
+        }); 
+        //console.log(routeResponse2.data.users);
+    }
+    });
+
+    const deleteVessel = (event) => {
+        const id = event.target.id;
+        //console.log(id);
+        axiosdeleteVessel(id);
+        setTimeout(axiosGetVessels, 1000);
+    };
+
+    const updateVessels = () => {axiosGetVessels();};
+
+    if(vesselArray.length > 0){
+        let listOfVessels = vesselArray.map(vessel =>
+          `<h5 class="card-title">${vessel.name} 
+          <button id="${vessel.name}" onClick={deleteVessel} class="webMasterList-button " style="float: right;">Delete</button> 
+          </h5>`
+        ).join('');
+    
         return (
-            <tr key={userData.name}>
-            <td onClick={() => viewFootballClub(footballClub.id)}>
-                <i class="School">{footballClub.school} </i>
-            </td>
-            <td data-label="Mascot"> {footballClub.mascot} </td>
-            <td onClick={() => editFootballClub(footballClub.id)}>
-                <i class="edit icon"></i>
-            </td>
-            <td onClick={() => deleteFootballClub(footballClub.id)}>
-                <i class="trash alternate icon"></i>
-            </td>
-            </tr>
+        <div className="page">
+            <button type="button" className="register-button" onClick={updateVessels}>Update</button>
+            <h3 class="h1-responsive font-weight-bold text-center my-4">________________</h3>
+            <h3 class="h1-responsive font-weight-bold text-center my-4">List of Vessels</h3>
+            <h3 class="h1-responsive font-weight-bold text-center my-4">________________</h3>
+          <table border = "0" cellPadding = "25" cellSpacing = "10"> 
+            <div  onClick={deleteVessel} dangerouslySetInnerHTML={{__html: listOfVessels}}></div>
+          </table>
+        </div>);
+    }
+    else{
+        return(
+            <div>
+                <br></br>
+            <button type="button" className="register-button" onClick={updateVessels}>Update</button>
+            <br></br>
+            <div>Click "Update" to show any updated vessels from the database.</div>
+            </div>
         );
-        })
-    : props.filteredClubs.map((footballClub) => {
-        return (
-            <tr key={footballClub.id}>
-            <td onClick={() => viewFootballClub(footballClub.id)}>
-                <i class="School">{footballClub.school} </i>
-            </td>
-            <td data-label="Mascot"> {footballClub.mascot} </td>
-            <td onClick={() => editFootballClub(footballClub.id)}>
-                <i class="edit icon"></i>
-            </td>
-            <td onClick={() => deleteFootballClub(footballClub.id)}>
-                <i class="trash alternate icon"></i>
-            </td>
-            </tr>
-        );
-        });
-    return <tbody>{buildingList}</tbody>;*/
+    }
 }
 export default WebMasterList;
